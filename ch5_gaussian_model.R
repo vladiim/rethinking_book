3372856069560f6e5734ab61f37abe70d2bc84be
Vladimir Mehakovic <vlad@Vlads-MacBook-Air.local>
2023-10-01 08:58:02 +1100

    Brute force posterior

--- ch5_gaussian_model.R
+++ ch5_gaussian_model.R
@@ -25,3 +25,53 @@ plot.prior.predictive <- function() {
   prior_h <- rnorm(1e4, sample_mu, sample_sigma)
   dens(prior_h) 
 }
+
+OLDheight.grid.posterior.brute.force <- function() {
+  mu <- seq(from=150, to=160, length.out=100)
+  sigma <- seq(from=7, to=9, length.out=100)
+  expand.grid(mu=mu, sigma=sigma) %>%
+    mutate(
+     ll = sapply(1:nrow(.), function(i) {
+        dnorm(height.adult()$height, .[i, "mu"], .[i, "sigma"], log = TRUE) %>%
+        prod()
+      })
+    ) %>%
+    mutate(
+      prior = ll + dnorm(mu, 178, 20) + dunif(sigma, 0, 50),
+      post = exp(prior - max(prior))
+    )
+}
+
+height.grid.posterior.brute.force <- function() {
+  mu <- seq(from=150, to=160, length.out=100)
+  sigma <- seq(from=7, to=9, length.out=100)
+  expand.grid(mu = mu, sigma = sigma) %>%
+  mutate(
+    ll = sapply(1:nrow(.), function(i) {
+      dnorm(height.adult()$height, d$mu[i], d$sigma[i], log = TRUE) %>%
+      sum()
+    }),
+    prod = ll + dnorm(mu, 178, 20, TRUE) + dunif(sigma, 0, 50, TRUE),
+    prob = exp(prod - max(prod))
+  )
+}
+
+height.grid.posterior.plots <- function() {
+  # height.grid.posterior.brute.force() %>% contour_xyz()
+  height.grid.posterior.brute.force() %>% image_xyz()
+}
+
+height.grid.posterior.sample.posterior <- function() {
+  d <- height.grid.posterior.brute.force()
+  sample.rows <- sample(1:nrow(d), size = 1e4, replace = TRUE, prob = d$prob)
+
+  data.frame(
+    mu = d$mu[ sample.rows ],
+    sigma = d$sigma[ sample.rows ]
+  )
+}
+
+height.grid.posterior.sample.posterior.plot <- function() {
+  d <- height.grid.posterior.sample.posterior()
+  plot(d$mu, d$sigma, cex = 0.5, pch = 15, col = col.alpha(rangi2, 0.1))
+}
