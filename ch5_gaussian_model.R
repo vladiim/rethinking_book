edbb875e10ec6855fe23f082ba2e7e2349dcc288
Vladimir Mehakovic <701194+vladiim@users.noreply.github.com>
2023-10-06 08:44:36 +1100

    ch5

--- ch5_gaussian_model.R
+++ ch5_gaussian_model.R
@@ -26,29 +26,13 @@ plot.prior.predictive <- function() {
   dens(prior_h) 
 }
 
-OLDheight.grid.posterior.brute.force <- function() {
-  mu <- seq(from=150, to=160, length.out=100)
-  sigma <- seq(from=7, to=9, length.out=100)
-  expand.grid(mu=mu, sigma=sigma) %>%
-    mutate(
-     ll = sapply(1:nrow(.), function(i) {
-        dnorm(height.adult()$height, .[i, "mu"], .[i, "sigma"], log = TRUE) %>%
-        prod()
-      })
-    ) %>%
-    mutate(
-      prior = ll + dnorm(mu, 178, 20) + dunif(sigma, 0, 50),
-      post = exp(prior - max(prior))
-    )
-}
-
 height.grid.posterior.brute.force <- function() {
   mu <- seq(from=150, to=160, length.out=100)
   sigma <- seq(from=7, to=9, length.out=100)
   expand.grid(mu = mu, sigma = sigma) %>%
   mutate(
     ll = sapply(1:nrow(.), function(i) {
-      dnorm(height.adult()$height, d$mu[i], d$sigma[i], log = TRUE) %>%
+      dnorm(height.adult()$height, mu[i], sigma[i], log = TRUE) %>%
       sum()
     }),
     prod = ll + dnorm(mu, 178, 20, TRUE) + dunif(sigma, 0, 50, TRUE),
@@ -61,9 +45,11 @@ height.grid.posterior.plots <- function() {
   height.grid.posterior.brute.force() %>% image_xyz()
 }
 
-height.grid.posterior.sample.posterior <- function() {
-  d <- height.grid.posterior.brute.force()
-  sample.rows <- sample(1:nrow(d), size = 1e4, replace = TRUE, prob = d$prob)
+height.grid.posterior.sample.posterior <- function(
+  d = height.grid.posterior.brute.force(),
+  size = 1e4
+  ) {
+  sample.rows <- sample(1:nrow(d), size = size, replace = TRUE, prob = d$prob)
 
   data.frame(
     mu = d$mu[ sample.rows ],
@@ -71,7 +57,18 @@ height.grid.posterior.sample.posterior <- function() {
   )
 }
 
-height.grid.posterior.sample.posterior.plot <- function() {
-  d <- height.grid.posterior.sample.posterior()
+height.grid.posterior.sample.posterior.plot <- function(d = height.grid.posterior.sample.posterior()) {
+  #plot(s$mu, s$sigma, cex = 0.5, pch = 15, col = col.alpha(rangi2, 0.1))
   plot(d$mu, d$sigma, cex = 0.5, pch = 15, col = col.alpha(rangi2, 0.1))
+}
+
+height.grid.posterior.sample.posterior.describe <- function(d = height.grid.posterior.sample.posterior()) {
+  par(mfrow = c(1, 2))
+  dens(d$mu)
+  dens(d$sigma)
+}
+
+height.grid.posterior.sample.posterior.PI <- function(d = height.grid.posterior.sample.posterior()) {
+  PI(d$mu)
+  PI(d$sigma)
 }
