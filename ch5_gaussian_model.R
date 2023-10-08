4366428a27ce56794858be37f0725c127544a73b
Vladimir Mehakovic <701194+vladiim@users.noreply.github.com>
2023-10-09 06:29:30 +1100

    Smaller sample

--- ch5_gaussian_model.R
+++ ch5_gaussian_model.R
@@ -41,7 +41,7 @@ height.grid.posterior.brute.force <- function() {
 }
 
 height.grid.posterior.plots <- function() {
-  # height.grid.posterior.brute.force() %>% contour_xyz()
+ # height.grid.posterior.brute.force() %>% contour_xyz()
   height.grid.posterior.brute.force() %>% image_xyz()
 }
 
@@ -71,4 +71,18 @@ height.grid.posterior.sample.posterior.describe <- function(d = height.grid.post
 height.grid.posterior.sample.posterior.PI <- function(d = height.grid.posterior.sample.posterior()) {
   PI(d$mu)
   PI(d$sigma)
+}
+
+height.grid.smaller.sample <- function(d = height.adult()) {
+  d3 <- sample(d$height, size=20)
+  mu.list <- seq( from=150, to=170, length.out=200)
+  sigma.list <- seq( from=4, to=20, length.out=200)
+  post2 <- expand.grid( mu=mu.list, sigma=sigma.list)
+  post2$LL <- sapply( 1:nrow(post2), function(i)  sum( dnorm( d3, mean=post2$mu[i], sd=post2$sigma[i],  log=TRUE)))
+  post2$prod <- post2$LL + dnorm( post2$mu, 178, 20, TRUE) +  dunif( post2$sigma, 0, 50, TRUE)
+  post2$prob <- exp( post2$prod - max(post2$prod))
+  sample2.rows <- sample( 1:nrow(post2), size=1e4, replace=TRUE,  prob=post2$prob)
+  sample2.mu <- post2$mu[ sample2.rows]
+  sample2.sigma <- post2$sigma[ sample2.rows]
+  plot( sample2.mu, sample2.sigma, cex=0.5,  col=col.alpha(rangi2,0.1),  xlab="mu", ylab="sigma", pch=16)
 }
