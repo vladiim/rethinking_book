612ae3f33c334e849b5692cebebea0442717702a
Vladimir Mehakovic <701194+vladiim@users.noreply.github.com>
2023-10-30 05:59:04 +1100

    ch4 quap wip

--- ch5_quap.R
+++ ch5_quap.R
@@ -2,23 +2,31 @@
 # Making inferences about the shape of the posterior
 # lies at the Maximum A Posteriori est (MAP)
 
-height.adult.modelDefinition <- function(stdiv = 20) {
+height.adult.modelDefinition <- function() {
+  # stdiv high = 20, low = 0.1
+
   alist(
     height ~ dnorm( mu, sigma ),
-    mu     ~ dnorm( 178, stdiv ),
+    mu     ~ dnorm( 178, 0.1 ),
     sigma  ~ dunif( 0, 50 )
   )
 }
 
-height.adult.modelFit <- function(d = height.adult(), stdiv = 20) {
-  height.adult.modelDefinition(stdiv) %>%
-    quap(d)
+height.adult.modelFit <- function(d = height.adult()) {
+  height.adult.modelDefinition() %>% quap(d)
 }
 
 height.adult.model.posteriorDist <- function() {
-  precis(height.adult.modelFit())
+  height.adult.modelFit() %>% precis()
 }
 
-height.adult.model.posteriorDist.lowSD <- function() {
-  precis(height.adult.modelFit(height.adult(), 0.1))
+# Variance - covariance matrix
+# Multi-dimensional glue for quadratic approximation
+# How each parameter relates to each other
+height.adult.varCovar <- function() {
+  height.adult.model.posteriorDist() %>% vcov()
+}
+
+height.adult.genSamples <- function() {
+  height.adult.modelFit() %>% extract.samples(, n = 1e4)
 }
